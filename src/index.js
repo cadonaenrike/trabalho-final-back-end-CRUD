a7ff301531acfc647a2f3877c9700e8d40847367
Everton cadona <cadonaenrike@gmail.com>
2023-05-06 02:14:41 -0300

    padronizei os codigos e espaçamentos e criei topicos parão para melhor leitura do codigo

--- src/index.js
+++ src/index.js
@@ -1,149 +1,135 @@
 import  express, { request } from "express";
-
-
 const app = express();
-
 const bcrypt = require('bcrypt');
-
 app.use(express.json());
 
-
-
-//array geral!!!
+//array geral!!! //
 let usuarios = [];
-let logado = false
+let logado = false;
 
-//esse e nosso midlaware que valida se tem id no array usuario para criar um recado
-function validaPeloId(request,response,next){
-    const usuarioId = request.params.id;
-    const usuarioValidado = usuarios.findIndex(usuario=> usuario.id === Number(usuarioId));
-    if(usuarioValidado != -1){
-        next()
-    }else{
-        return response.status(404).send("Nao encontrado")
-    }
-};
-
-function validaLogin(request, response, next) {
-
-  if (usuarios.length > 0 && usuarios[0].logado === true) {
-    // Se estiver logado, chama o próximo middleware ou rota
-    next();
-  } else {
-    // Se não estiver logado, envia uma resposta com status 401 (Não Autorizado)
-    response.status(401).send("Não Logado");
-  }
-}
-
-//ele vai criar nosso usuario
-// utilizando metodo post
-
-app.post('/usuarios',async (request,response)=>{
-    let { nome,email,senha}= request.body;
-    let validasetemusuariocadastrado = usuarios.find(user => user.email === email)
-    if(validasetemusuariocadastrado){
-        console.log("ja possui login")
-        response.status(409).send("usuario ja existente")
-    }else{
-        let id = Math.floor(Math.random()*6767);
-        let senhaCriptografada = await bcrypt.hash(senha, 10);
-        console.log(senhaCriptografada)
-        let novoUsuario = {id, nome, email, senhaCriptografada,logado,recado:[]};
-        usuarios.push(novoUsuario);
-        response.status(201).send("criado com sucesso");
-    }
-})
-
-// criando login por post
-app.post('/usuarios/login', async (request, response) => {
-  let { email, senha } = request.body;
-  let emailVerificado = usuarios.find(user => user.email === email);
-  
-  if (!emailVerificado) {
-    return response.status(401).send('Email ou senha inválidos');
-  }
-  
-  let senhaComparada = await bcrypt.compare(senha, emailVerificado.senhaCriptografada);
-  
-  if (!senhaComparada) {
-    return response.status(401).send('Email ou senha inválidos');
-  }else{ 
-    function encontrarIndiceDoObjeto(arayPai, logado, valor) {
-      for (let i = 0; i < usuarios.length; i++) {
-        if (arayPai[i][logado] === valor) {
-          return i; // Retorna o índice quando encontrar o objeto com a propriedade e valor desejados
-        }else{
-          return -1;
-        }
+//esse e nosso midlaware que valida se tem id no array usuario para criar um recado //
+  function validaPeloId(request,response,next){
+      const usuarioId = request.params.id;
+      const usuarioValidado = usuarios.findIndex(usuario=> usuario.id === Number(usuarioId));
+      if(usuarioValidado != -1){
+          next()
+      }else{
+          return response.status(404).send("Nao encontrado")
       }
+  };
+
+//midlaware que verifica se esta logado no sistema //
+  function validaLogin(request, response, next) {
+
+    if (usuarios.length > 0 && usuarios[0].logado === true) {
+      // Se estiver logado, chama o próximo middleware ou rota
+      next();
+    } else {
+      // Se não estiver logado, envia uma resposta com status 401 (Não Autorizado)
+      response.status(401).send("Não Logado");
     }
-    const indice = encontrarIndiceDoObjeto(usuarios, "logado", false);
-    if (indice !== -1){
-      usuarios[indice].logado = true;
-    }
-    response.status(202).send('Logado com sucesso')
-    
-   }
-});
+  };
 
-
-//criamos os recados no array de usuarios
-
-app.post("/usuarios/:id/recado",validaPeloId,validaLogin, (request,response)=>{
-    const novoRecado = request.body;
-    let recadoCriado = {
-        id: Math.floor(Math.random()*1425),
-        titulo: novoRecado.titulo,
-        descricao: novoRecado.descricao
-    };
-    const id = request.params.id;
-
-    let pegueiIdParaRecado = usuarios.findIndex(usuario=> usuario.id === Number(id));
-    console.log(pegueiIdParaRecado);
-
-    usuarios[pegueiIdParaRecado].recado.push(recadoCriado);
-
-   return response.status(201).send("Recado criado com sucesso")
-})
-
-//vamos ler quem ta dentro do array 
-// usando o get //
-
-app.get('/usuarios',validaLogin, (request,response)=>{
-    response.status(202).json(usuarios);
-})
-
-//vamos alterar o recado do usuario //
-// usando o put //
-app.put('/usuarios/:id/recado/:recadoId',validaLogin, (req, res) => {
-    const usuarioId = req.params.id; // obtém o ID do usuário a partir da URL
-    const recadoId = req.params.recadoId; // obtém o ID do recado a partir da URL
-  
-    // Encontra o usuário pelo ID
-    const usuario = usuarios.find(user => user.id === Number(usuarioId));
-
-    // Encontra o recado pelo ID
-    const recado = usuario.recado.find(recado => recado.id === Number(recadoId));
-  
-    if (!recado) {
-      return res.status(404).send('Recado não encontrado');
-    }
-  
-    // Atualiza o recado
-    recado.titulo = req.body.titulo;
-    recado.descricao = req.body.descricao;
-  
-    res.status(200).send('Recado atualizado com sucesso');
+//ele vai criar nosso usuario// utilizando metodo post //
+  app.post('/usuarios',async (request,response)=>{
+      let { nome,email,senha}= request.body;
+      let validasetemusuariocadastrado = usuarios.find(user => user.email === email)
+      if(validasetemusuariocadastrado){
+          console.log("ja possui login")
+          response.status(409).send("usuario ja existente")
+      }else{
+          let id = Math.floor(Math.random()*6767);
+          let senhaCriptografada = await bcrypt.hash(senha, 10);
+          console.log(senhaCriptografada)
+          let novoUsuario = {id, nome, email, senhaCriptografada,logado,recado:[]};
+          usuarios.push(novoUsuario);
+          response.status(201).send("criado com sucesso");
+      }
   });
 
-  
-   // excluindo o recado por ID 
-  // utilizando o delete 
+//criando login // utilizando metodo post //
+  app.post('/usuarios/login', async (request, response) => {
+    let { email, senha } = request.body;
+    let emailVerificado = usuarios.find(user => user.email === email);
+    
+    if (!emailVerificado) {
+      return response.status(401).send('Email ou senha inválidos');
+    }
+    
+    let senhaComparada = await bcrypt.compare(senha, emailVerificado.senhaCriptografada);
+    
+    if (!senhaComparada) {
+      return response.status(401).send('Email ou senha inválidos');
+    }else{ 
+      function encontrarIndiceDoObjeto(arayPai, logado, valor) {
+        for (let i = 0; i < usuarios.length; i++) {
+          if (arayPai[i][logado] === valor) {
+            return i; // Retorna o índice quando encontrar o objeto com a propriedade e valor desejados
+          }else{
+            return -1;
+          }
+        }
+      }
+      const indice = encontrarIndiceDoObjeto(usuarios, "logado", false);
+      if (indice !== -1){
+        usuarios[indice].logado = true;
+      }
+      response.status(202).send('Logado com sucesso')
+      
+    }
+  });
 
+//vamos ler quem ta dentro do array // utilizando metodo get //
+  app.get('/usuarios',validaLogin, (request,response)=>{
+    response.status(202).json(usuarios);
+  })
+
+//criamos os recados no array de usuarios// utilizando metodo post //
+  app.post("/usuarios/:id/recado",validaPeloId,validaLogin, (request,response)=>{
+      const novoRecado = request.body;
+      let recadoCriado = {
+          id: Math.floor(Math.random()*1425),
+          titulo: novoRecado.titulo,
+          descricao: novoRecado.descricao
+      };
+      const id = request.params.id;
+
+      let pegueiIdParaRecado = usuarios.findIndex(usuario=> usuario.id === Number(id));
+      console.log(pegueiIdParaRecado);
+
+      usuarios[pegueiIdParaRecado].recado.push(recadoCriado);
+
+    return response.status(201).send("Recado criado com sucesso")
+  });
+
+//vamos alterar o recado do usuario // utilizando metodo o put //
+  app.put('/usuarios/:id/recado/:recadoId',validaLogin, (req, res) => {
+      const usuarioId = req.params.id; // obtém o ID do usuário a partir da URL
+      const recadoId = req.params.recadoId; // obtém o ID do recado a partir da URL
+    
+      // Encontra o usuário pelo ID
+      const usuario = usuarios.find(user => user.id === Number(usuarioId));
+
+      // Encontra o recado pelo ID
+      const recado = usuario.recado.find(recado => recado.id === Number(recadoId));
+    
+      if (!recado) {
+        return res.status(404).send('Recado não encontrado');
+      }
+    
+      // Atualiza o recado
+      recado.titulo = req.body.titulo;
+      recado.descricao = req.body.descricao;
+    
+      res.status(200).send('Recado atualizado com sucesso');
+    });
+
+// excluindo o recado por ID // utilizando metodo delete //
   app.delete('/usuarios/:id/recado/:idRecado',validaLogin,  (request, response)=>{
     const id = Number(request.params.id);
     const idRecado = Number(request.params.idRecado);
     const indexDoUsuario = usuarios.findIndex(usuario => usuario.id === id);
+
     //verifica se tem o id no array usuarios
         if (indexDoUsuario < 0) {
         response.status(404).send("Usuário não encontrado.");
@@ -158,15 +144,12 @@ app.put('/usuarios/:id/recado/:recadoId',validaLogin, (req, res) => {
         response.status(404).send("Recado não encontrado.");
         return;
         }
-    
+
     //pega o id do array recado no array usuarios e exclui    
     usuarios[indexDoUsuario].recado.splice(indexDoRecado, 1);
-    
     response.status(202).send("Recado excluído com sucesso.");
-  })
+  });
   
-
-
-app.listen(1425, ()=>{
-    console.log("servidor rodando")
-});
+  app.listen(1425, ()=>{
+      console.log("servidor rodando")
+  });
